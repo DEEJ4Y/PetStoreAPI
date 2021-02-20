052bdf15b51dcde6855e12bdcd3ca58b64533b3e
davidjosephind <davidjosephind@gmail.com>
2021-02-20 17:54:13 +0530

    Spin up server

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ const { connect } = require("mongoose");
 const connectDB = require("./config/db");
 
 connectDB();
+
+const app = express();
+
+// Body Parser
+app.use(express.json());
+
+const PORT = process.env.PORT;
+const server = app.listen(
+  PORT,
+  console.log(`Server is running on port ${PORT}`)
+);
