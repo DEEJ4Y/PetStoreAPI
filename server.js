b55fd853083bef1b91f6d8d2b4d9f3b09e74cbdf
davidjosephind <davidjosephind@gmail.com>
2021-02-21 09:41:10 +0530

    Add Heroku config

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config({ path: "./config/config.env" });
+require("dotenv").config();
 const express = require("express");
 const { connect } = require("mongoose");
 const connectDB = require("./config/db");
